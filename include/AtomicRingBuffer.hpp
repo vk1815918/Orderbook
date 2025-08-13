c4000716294d18dc84711fe3a585ff60a4a67bbc
vk1815918 <viswaretaskotra@gmail.com>
2025-08-12 23:17:05 -0500

    added atomicringbuffer basic functions

--- include/AtomicRingBuffer.hpp
+++ include/AtomicRingBuffer.hpp
@@ -1,1 +1,58 @@
+#pragma once
+
+#include <atomic>
+#include <cstddef>
+
+
+template<typename T>
+class AtomicRingBuffer {
+
+private:
+
+    std::atomic<size_t> head;
+    std::atomic<size_t> tail;
+
+    const size_t capacity;
+    const size_t mask;
+    T* buffer;
+
+public:
+
+    AtomicRingBuffer(size_t size) : head(0), tail(0), capacity(size) {
+        buffer = new T[size];
+    }
+
+    bool push(const T& input) {
+        if (full()) return false;
+
+        size_t current_tail = tail.load();
+        buffer[current_tail] = input;
+        tail.store((current_tail + 1) % capacity);
+        return true;
+    } 
+
+    bool pop(T& input) {
+        if (empty()) return false;
+
+        size_t current_head = head.load();
+        input = buffer[current_head];
+        head.store((current_head + 1) % capacity);
+        
+        return true;
+    }
+
+    bool full() {
+        size_t next_tail = (tail.load() + 1) % capacity;
+        return (next_tail == head.load());
+    }
+
+    bool empty() {
+        if (head.load() == tail.load()) return true;
+        return false;
+    }
+
+
+
+
+};
 
--- include/Order.hpp
+++ include/Order.hpp
@@ -1,30 +1,13 @@
 #pragma once
-// #include <string>
-// #include <chrono>
 #include <cstdint>
-
-enum class Side : uint8_t {
-    BUY = 0,
-    SELL = 1
-};
-
-enum class OrderType : uint8_t {
-    LIMIT = 0,
-    MARKET = 1
-};
 
 struct Order {
 
     uint64_t id;
-    uint64_t timestamp;
-    uint32_t quantity;
+    uint64_t timestamp_ns; // nanoseconds since epoch
+    uint32_t quantity; // 
     uint32_t remaining;
     uint32_t price;
-
-    Side side;
-    OrderType type;
-
-    Order* next = nullptr;
-    Order* prev = nullptr;
+    uint8_t side;
 
 };
